799b07eef14c6d956faeb7cb1c0362860d7535a7
nickbenz007 <nickbenz007@gmail.com>
2024-06-08 01:24:36 +0500

    server url replaced

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://bamboo-3d-shirt.vercel.app/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://bamboo-3d-shirt.vercel.app/api/v1/dalle",
   },
 };
 
